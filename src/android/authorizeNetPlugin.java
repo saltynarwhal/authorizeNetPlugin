4886c963752be0c22a80b2bb08c4b44656556d29
jordan-strong <jstrong3@byu.edu>
2018-12-20 15:29:33 -0700

    fixin' error

--- src/android/authorizeNetPlugin.java
+++ src/android/authorizeNetPlugin.java
@@ -22,6 +22,7 @@ import net.authorize.Merchant;
 import net.authorize.TransactionType;
 import net.authorize.aim.cardpresent.DeviceType;
 import net.authorize.aim.cardpresent.MarketType;
+import net.authorize.aim.emv.iF;
 import net.authorize.aim.emv.EMVErrorCode;
 import net.authorize.aim.emv.EMVTransaction;
 import net.authorize.aim.emv.EMVTransactionManager;
